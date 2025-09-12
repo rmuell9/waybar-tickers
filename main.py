fb79ab5046b0848f3ed10ca0b27181fdad5f97e2
rmuell9 <rmatthewmueller@gmail.com>
2025-09-12 18:36:58 -0500

    formatting

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import yfinance as yf
 import sys
 
+
 tickerSymbol = sys.argv[1]
 ticker = yf.Ticker(tickerSymbol)
 lastClose = ticker.info['previousClose']
 current = ticker.info['currentPrice']
 change = current - lastClose
 
+price = tickerSymbol + ' $' + str(current)
+
 if change < 0:
     sign = '-'
-else:
+elif change > 0:
     sign = '+'
+else:
+    print(price)
 
-
-price = tickerSymbol + ' $' + str(current)
 priceChange = f' {sign}$' + str(round(abs(change), 2))
 output = price + priceChange
 
